0c0681f126afd0b1810d0a1d950323871dcba58b
brooklvk <brooklvk@byui.edu>
2022-11-05 14:27:37 -0600

    trying to figure things out

--- Color.cs
+++ Color.cs
@@ -1,4 +1,5 @@
 // Assigns a random color from a list that gems and rocks can generate as
 
 class Color {
-  }
+
+}
--- Shape.cs
+++ Shape.cs
@@ -8,7 +8,7 @@ class Shape
 {
     //Brooklyn can't figure this out. but I was trying to figure out a way to make it work 
     // without inheriting from the Location class. To be more simple. 
-    Random random = new Random();
-    Shape circle = DrawCircle($"{Random.Next(0, 1800)}", -5, 0, WHITE);
+    Random random = new Random;
+    Shape circle = DrawCircle($"{Random(0, 1800)}", -5, 0, WHITE);
     Shape rectangle = DrawRectangle($"{randValue}", -5, 6, 4, GREEN); 
 }
